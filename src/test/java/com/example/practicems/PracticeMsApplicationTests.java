e067a62a45ffe4c60904a07d363bbc48f92ff8dd
Moin Shaikh <moin241397@gmail.comm>
2023-10-11 21:53:50 +0530

    new code added

---
+++ src/test/java/com/example/practicems/PracticeMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practicems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
