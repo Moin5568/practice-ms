b9a50ee354f2f8836bcca72c5fddccb0e9966307
Moin Shaikh <moin241397@gmail.comm>
2023-10-12 10:42:34 +0530

    code modified

--- src/main/java/com/example/practicems/flight.java
+++ src/main/java/com/example/practicems/flight.java
@@ -7,5 +7,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class flight {
     @GetMapping("/flight")
-    public String getData () {return "hello" ;}
+    public String getData () {return "hello I am Moin" ;}
 }
