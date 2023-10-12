f91f7fdc1f4e3e75249c884296779f7d6f2e1f30
Moin Shaikh <moin241397@gmail.comm>
2023-10-12 11:18:56 +0530

    chnages are pushing

---
+++ src/main/java/com/example/practicems/bus.java
@@ -0,0 +1,11 @@
+package com.example.practicems;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class bus {
+    @GetMapping("/bus")
+    public String getData () {return "hello I am Moin book a bus for travelling " ;}
+}
